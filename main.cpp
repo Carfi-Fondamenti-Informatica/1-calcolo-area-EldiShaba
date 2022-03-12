8098811bd731b3ff4606d8afa93ed048b1a96dc6
EldiShaba <101250259+EldiShaba@users.noreply.github.com>
2022-03-12 12:09:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
-using namespace std;
+   using namespace std;
 
-int main(){
-//Qui vanno definite le variabili
+   int main () {
+       float  a, b, c;
+   cout<<"Inserire valore i tre valori a, b, c:" <<endl;
+       cin>> a,b,c;
+       float triangolo = a*b/2;
+       float  quadrato= a*a;
+       float  rettangolo=a*b;
+       float  trapezio=(a+b)*c/2;
+   std::cout<< "Area triangolo= " <<  triangolo << " Area quadrato: " <<  quadrato << " Area rettangolo: " << rettangolo << " Area trapezio: "<< trapezio<< std::endl;
+   return 0;
+   }
 
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
 
 
-}

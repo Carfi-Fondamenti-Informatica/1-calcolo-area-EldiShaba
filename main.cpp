8e08bbfd90f610e4ee7e6fec6b5271e88d247dc3
EldiShaba <101250259+EldiShaba@users.noreply.github.com>
2022-03-15 15:15:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
-   using namespace std;
-
-   int main () {
-       float  a, b, c;
-   cout<<"Inserire valore i tre valori a, b, c:" <<endl;
-       cin>> a,b,c;
-       float triangolo = a*b/2;
-       float  quadrato= a*a;
-       float  rettangolo=a*b;
-       float  trapezio=(a+b)*c/2;
-   std::cout<< "Area triangolo= " <<  triangolo << " Area quadrato: " <<  quadrato << " Area rettangolo: " << rettangolo << " Area trapezio: "<< trapezio<< std::endl;
+   int main ()
+   {
+       float  a; float b; float c;
+       float Atriangolo=((a*b)/2); float Aquadrato=(a*a);float Arettangolo=(a*b); float Atrapezio=(((a+b)*c)/2);
+       std::cout<< ((a*b)/2)<<std::endl;
+   std::cout<< (a*a) <<std::endl;
+   std::cout<< (a*b) <<std::endl;
+   std::cout<< (((a+b)*c)/2)<< std::endl;
+   std::cin>>a>>b>>c;
    return 0;
    }
-
-
-
